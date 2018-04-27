11fd5a8a1d84f6df794f23bd9bf585061a59d51e
Sam Ringleman <sam.ringleman@gmail.com>
2018-04-27 14:39:54 -0600

    update the base controller to include all predefined crud operations, this will make adding controllers much easier

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use http\Env\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+
+    /**
+     * @property \Illuminate\Database\Eloquent\Model
+     */
+    public $modelName;
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        return response()->json($this->modelName::all());
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        return response()->json($this->modelName::find($id));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+        $model = $this->modelName::create($request->all());
+        return response()->json($model, 200);
+    }
+
+    /**
+     * @param $id
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update($id, Request $request)
+    {
+        $model = $this->modelName::findOrFail($id);
+        $model->update($request->all());
+        return response()->json($model, 200);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\Response|\Laravel\Lumen\Http\ResponseFactory
+     */
+    public function delete($id)
+    {
+        $this->modelName::findOrFail($id);
+        return response('Deleted Successfully', 200);
+    }
 }
---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+
+class PostsController extends Controller
+{
+    public $modelName = Post::class;
+
+}
